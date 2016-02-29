02b8d487984e147f11d9f4d061f461f8da0fe8b7
Ryan Ashcraft <ryan@ryanashcraft.me>
2016-02-28 20:55:48 -0800

    Optimize drawing
    Use pattern and only draw matches.

--- main.js
+++ main.js
@@ -13,43 +13,14 @@ var context = svg.append("g")
 
 // Load the JSON
 var transcriptFiles = [
-    'data/08_28_2012_Bud_Peterson.json',
-    'data/08_28_2013_Bud_Peterson.json',
-    'data/08_28_2014_Bud_Peterson.json',
-    'data/08_27_2015_Bud_Peterson.json'
+  'data/08_28_2012_Bud_Peterson.json',
+  'data/08_28_2013_Bud_Peterson.json',
+  'data/08_28_2014_Bud_Peterson.json',
+  'data/08_27_2015_Bud_Peterson.json'
 ];
-async.map(transcriptFiles, d3.json, function(error, data) {
-    drawAddresses(context, data, width, height);
-    updateCounts();
-});
+async.map(transcriptFiles, d3.json, onDataLoaded);
 
-function onInputChange(e) {
-  var phrase = $(this).val().split(" ")[0];
-
-  $("span#phrase").text(phrase);
-
-  d3.selectAll(".word").classed("selected", function(d, i) {
-    return phrase && d["word"].toLowerCase().indexOf(phrase.toLowerCase()) !== -1;
-  });
-
-  updateCounts();
-}
-
-$("input").bind("keyup", _.debounce(onInputChange, 300));
-
-function updateCounts() {
-  var addresses = d3.selectAll(".address");
-
-  addresses.each(function() {
-    var address = d3.select(this);
-    address.select("text.count")
-      .text(function(d) {
-        return address.selectAll(".selected").size();
-      });
-  });
-}
-
-function drawAddresses(svg, json, width, height) {
+function onDataLoaded(error, data) {
   var year = d3.scale.ordinal()
     .rangeRoundBands([0, width], 0.1);
 
@@ -57,21 +28,19 @@ function drawAddresses(svg, json, width, height) {
     .scale(year)
     .orient("top");
 
-  year.domain(json.map(function(d) { return d["metadata"]["date"]["year"]; }));
+  year.domain(data.map(function(d) { return d["metadata"]["date"]["year"]; }));
 
   svg.append("g")
     .attr("class", "year axis")
     .call(yearAxis);
 
   // Draw words now
-  json.forEach(function(addressData, addressIndex) {
+  data.forEach(function(addressData, addressIndex) {
     var wordCount = addressData["metadata"]["word count"];
     var lineWordCounts = addressData["metadata"]["line word counts"];
 
     var locations = addressData["locations"];
     var words = [];
-
-    var word_height = height / lineWordCounts.length;
 
     for (var word in locations) {
       var wordInstances = locations[word];
@@ -105,45 +74,105 @@ function drawAddresses(svg, json, width, height) {
       .domain(lines);
   });
 
-  var address = svg.selectAll("g.address")
-    .data(json)
-    .enter()
-    .append("g")
-    .attr("class", "address")
-    .attr("transform", function(d) {
-      return "translate(" + year(d["metadata"]["date"]["year"]) + ", 10)";
+  function drawMatches(isMatch) {
+    var words = d3.selectAll('.words');
+
+    words.selectAll('*')
+      .remove();
+
+    words.selectAll("rect")
+      .data(function(d) { return d["words"].filter(isMatch) })
+      .enter()
+      .append("rect")
+      .attr("class", function(d) { return "word-" + d["word"] + " word"; })
+      .attr("x", function(d) { return d["x"]; })
+      .attr("y", function(d) { return data[d["address index"]].y(d["y"]); })
+      .attr("width", function(d) { return d["width"]; })
+      .attr("height", function(d) { return data[d["address index"]].y.rangeBand(); });
+  }
+
+  function updateCounts(isMatch) {
+    var addresses = d3.selectAll(".address");
+
+    addresses.each(function() {
+      var address = d3.select(this);
+      address.select("text.count")
+        .text(function(d) {
+          return d.words.filter(isMatch).length;
+        });
     });
+  }
 
-  address.append("text")
-    .attr("x", year.rangeBand() / 2)
-    .attr("y", 0)
-    .attr("text-anchor", "middle")
-    .text(function(d) {
-      return d["metadata"]["last name"];
-    });
+  function drawAddresses(svg) {
+    var address = svg.selectAll("g.address")
+      .data(data)
+      .enter()
+      .append("g")
+      .attr("class", "address")
+      .attr("transform", function(d) {
+        return "translate(" + year(d["metadata"]["date"]["year"]) + ", 10)";
+      });
 
-  var words = address.append("g")
-    .attr("class", "words")
-    .attr("transform", function(d) {
-      return "translate(0, 25)";
-    });
+    address.append("text")
+      .attr("x", year.rangeBand() / 2)
+      .attr("y", 0)
+      .attr("text-anchor", "middle")
+      .text(function(d) {
+        return d["metadata"]["last name"];
+      });
 
-  words.selectAll("rect")
-    .data(function(d) { return d["words"]; })
-    .enter()
-    .append("rect")
-    .attr("class", function(d) { return "word-" + d["word"] + " word"; })
-    .classed("selected", function(d, i) {
-      return d["word"].toLowerCase() == $("input").val().toLowerCase();
-    })
-    .attr("x", function(d) { return d["x"]; })
-    .attr("y", function(d) { return json[d["address index"]].y(d["y"]); })
-    .attr("width", function(d) { return d["width"]; })
-    .attr("height", function(d) { return json[d["address index"]].y.rangeBand(); });
+    var pattern = address.append("pattern")
+      .attr("id", "Line")
+      .attr("patternUnits", "userSpaceOnUse")
+      .attr("width", year.rangeBand())
+      .attr("height", function(d) { return d.y.rangeBand() * 2; });
 
-  address.append("text")
-    .attr("class", "count")
-    .attr("x", year.rangeBand() / 2)
-    .attr("y", 20)
-    .attr("text-anchor", "middle");
-}
+    pattern.append("rect")
+      .attr("x", 0)
+      .attr("y", function(d) { return d.y.rangeBand(); })
+      .attr("fill", "#ddd")
+      .attr("width", year.rangeBand())
+      .attr("height", function(d) { return d.y.rangeBand(); });
+
+    address.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", year.rangeBand())
+      .attr("height", "100%")
+      .attr("transform", "translate(0, 25)")
+      .attr("fill", "url(#Line)");
+
+    var words = address.append("g")
+      .attr("class", "words")
+      .attr("transform", "translate(0, 25)");
+
+    address.append("text")
+      .attr("class", "count")
+      .attr("x", year.rangeBand() / 2)
+      .attr("y", 20)
+      .attr("text-anchor", "middle");
+  }
+
+  function onInputChange(e) {
+    var phrase = $(this).val().split(" ")[0];
+
+    $("span#phrase").text(phrase);
+
+    var isMatch = function isMatch(data) {
+      return !!(phrase && data.word.toLowerCase().indexOf(phrase.toLowerCase()) !== -1);
+    };
+
+    drawMatches(isMatch);
+    updateCounts(isMatch);
+  }
+
+  var isMatch = function isMatch(data) {
+    var phrase = $("input").val();
+    return !!(phrase && data.word.toLowerCase().indexOf(phrase.toLowerCase()) !== -1);
+  };
+
+  drawAddresses(context);
+  drawMatches(isMatch);
+  updateCounts(isMatch);
+  $("input").bind("keyup", _.debounce(onInputChange, 300));
+};
